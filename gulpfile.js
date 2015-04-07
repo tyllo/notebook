b7389905c9f9696e98bc7bff66e7a982a901116a
tyllo <gmtyllo@gmail.com>
2015-04-07 23:27:36 +1000

    промежуточный commit

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('build', function() {
   gulp.run('components/datetimepicker');
   gulp.run('components/jquery');
   gulp.run('components/modernizr');
-  gulp.run('components/foundation');
+  //gulp.run('components/foundation');
 	gulp.run('components/font-awesome');
 	//gulp.run('components/cropper');
 	gulp.run('src');
