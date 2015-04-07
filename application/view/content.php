d1e36d93a1cc2129ca2618ec9926cdc9cce7efdc
tyllo <gmtyllo@gmail.com>
2015-04-07 18:48:35 +1000

    промежуточный commit

--- application/view/content.php
+++ application/view/content.php
@@ -1,17 +1,19 @@
 
 <div class="row">
-  <br><!-- <div class="small-12 columns"><h3>Заголовок</h3></div> -->
-</div>
-<div class="row">
-  <ul class="small-block-grid-3 medium-block-grid-6 large-block-grid-8">
+  <div class="small-12 columns">
+    <br><!-- <div class="small-12 columns"><h3>Заголовок</h3></div> -->
+  </div>
+  <div class="small-12 columns">
+    <ul class="small-block-grid-3 medium-block-grid-6 large-block-grid-8">
 <?php foreach($contactArr as $contact) :?>
-    <li>
-      <a href="#" data-reveal-id="modal-show-user" data-id-user="<?=$contact['id']?>">
-        <img class="th" src="/images/user.png" />
-      </a>
-      <div class="hide-text"><?=$contact['name']?></div>
-      <div class="hide-text"><?=$contact['surname']?></div>
-    </li>
-<?php endforeach; ?>
-  </ul>
+      <li>
+        <a href="#" data-reveal-id="modal-show-user" data-id-user="<?=$contact['id']?>">
+          <img class="th" src="/images/user.png" />
+        </a>
+        <div class="hide-text"><?=$contact['name']?></div>
+        <div class="hide-text"><?=$contact['surname']?></div>
+      </li>
+ <?php endforeach; ?>
+    </ul>
+  </div>
 </div>
